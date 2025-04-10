ca410f562f7d4db450d3047dfc94b2e852ee6bc6
thabus <thaisdab@gmail.com>
2025-04-10 14:35:42 -0300

    testando

--- ecommerce/ecommerce/src/main/java/com/ecommerce_ap1/ecommerce/EcommerceApplication.java
+++ ecommerce/ecommerce/src/main/java/com/ecommerce_ap1/ecommerce/EcommerceApplication.java
@@ -1,9 +1,11 @@
 package com.ecommerce_ap1.ecommerce;
 
+import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCosmosRepositories(basePackages = ".com.ecommerce_ap1.ecommerce.repositories.cosmos")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
