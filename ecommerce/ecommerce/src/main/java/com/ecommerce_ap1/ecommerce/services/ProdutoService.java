0d40877fdbf1e27a8ca44237dcb8c751d6ed00cf
thabus <thaisdab@gmail.com>
2025-04-09 21:43:16 -0300

    fix: desligando required autowired

--- ecommerce/ecommerce/src/main/java/com/ecommerce_ap1/ecommerce/services/ProdutoService.java
+++ ecommerce/ecommerce/src/main/java/com/ecommerce_ap1/ecommerce/services/ProdutoService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class ProdutoService {
 
-    @Autowired
+    @Autowired(required = false)
     private ProdutoRepository produtoRepository;
 
     public Produto criarProduto(Produto produto) {
